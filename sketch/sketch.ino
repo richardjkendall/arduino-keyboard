9140be8074b8fa656337fc35dba9c1b3b5215b77
Richard Kendall <richard.kendall@gmail.com>
2020-04-06 21:40:00 +1000

    very basic version with three buttons, plays C6, D6, E6

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -14,29 +14,63 @@ CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10 megohm resistor 
 CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);        // 10 megohm resistor between pins 4 & 6, pin 6 is sensor pin, add wire, foil
 CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10 megohm resistor between pins 4 & 8, pin 8 is sensor pin, add wire, foil
 
+int THRESHOLD = 2000;
+
 void setup()                    
 {
-
-   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-   Serial.begin(9600);
+   pinMode(A0, OUTPUT);
+   pinMode(A1, OUTPUT);
+   pinMode(A2, OUTPUT);
+   //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   //Serial.begin(9600);
 
 }
 
 void loop()                    
 {
-    long start = millis();
+    //long start = millis();
     long total1 =  cs_4_2.capacitiveSensor(30);
     long total2 =  cs_4_5.capacitiveSensor(30);
     long total3 =  cs_4_8.capacitiveSensor(30);
 
-    Serial.print(millis() - start);        // check on performance in milliseconds
+    /*Serial.print(millis() - start);        // check on performance in milliseconds
     Serial.print("\t");                    // tab character for debug window spacing
 
-    Serial.print(total1);                  // print sensor output 1
+    Serial.print(total1 > 4000 ? "p" : "n");                  // print sensor output 1
     Serial.print("\t");
-    Serial.print(total2);                  // print sensor output 2
+    Serial.print(total2 > 4000 ? "p" : "n");                  // print sensor output 2
     Serial.print("\t");
-    Serial.println(total3);                // print sensor output 3
+    Serial.println(total3 > 4000 ? "p" : "n");                // print sensor output 3
 
-    delay(3);                             // arbitrary delay to limit data to serial port 
+    delay(3);                             // arbitrary delay to limit data to serial port */
+
+    digitalWrite(A0, total1 > THRESHOLD ? HIGH : LOW);
+    digitalWrite(A1, total2 > THRESHOLD ? HIGH : LOW);
+    digitalWrite(A2, total3 > THRESHOLD ? HIGH : LOW);
+
+    bool t = false;
+    if(total1 > THRESHOLD) {
+      t = true;
+      tone(10, 1046, 100);
+    } else {
+      noTone(10);
+    }
+
+    if(total2 > THRESHOLD) {
+      t = true;
+      tone(10, 1175, 100);
+    } else {
+      if(!t) {
+        noTone(10);
+      }
+    }
+
+    if(total3 > THRESHOLD) {
+      t = true;
+      tone(10, 1318, 100);
+    } else {
+      if(!t) {
+        noTone(10);
+      }
+    }
 }
